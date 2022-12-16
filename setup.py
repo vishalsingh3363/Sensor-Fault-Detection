e4994c2f3b7c127f0052992222fec5c9724b06ec
vishalsingh <tan.vishalsingh@gmail.com>
2022-12-16 12:54:34 +0530

    structure set up done

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages,setup
+
+setup(
+    name="sensor",
+    version="0.0.1",
+    author="vishal",
+    author_email="tan.vishalsingh@gmail.com",
+    packages = find_packages(),
+)
