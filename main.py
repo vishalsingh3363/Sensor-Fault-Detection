bffb0a089ecc53443e0dd3feaca24f96ea187dd2
vishalsingh <tan.vishalsingh@gmail.com>
2022-12-23 17:29:07 +0530

    updated components

--- main.py
+++ main.py
@@ -5,12 +5,20 @@ from sensor.logger import logging
 #from sensor.entity.config_entity import TrainingPipelineConfig,DataIngestionConfig
 from sensor.pipeline import training_pipeline
 from sensor.pipeline.training_pipeline import TrainPipeline
+from sensor.utils.main_utils import read_yaml_file
 
+def set_env_variable(env_file_path):
+    env_config= read_yaml_file(env_file_path)
+    os.environ['MONGO_DB_URL']= env_config['MONGO_DB_URL']
 
 if __name__ == '__main__':
     try:
-        training_pipeline= TrainPipeline()
+        env_file_path='/config/workspace/env.yaml'
+        set_env_variable(env_file_path)
+        training_pipeline=TrainPipeline()
         training_pipeline.run_pipeline()
+        #training_pipeline= TrainPipeline()
+        #training_pipeline.run_pipeline()
         #training_pipeline_config= TrainingPipelineConfig()
         #data_ingestion_config= DataIngestionConfig(training_pipeline_config= training_pipeline_config)
         #print(data_ingestion_config.__dict__)
--- sensor/ml/model/estimator.py
+++ sensor/ml/model/estimator.py
@@ -14,6 +14,26 @@ class TargetValueMapping:
 
 
 
+#Write a code to train model and check the accuracy.
+
+class SensorModel:
+
+    def __init__(self,preprocessor,model):
+        try:
+            self.preprocessor = preprocessor
+            self.model = model
+        except Exception as e:
+            raise e
+    
+    def predict(self,x):
+        try:
+            x_transform = self.preprocessor.transform(x)
+            y_hat = self.model.predict(x_transform)
+            return y_hat
+        except Exception as e:
+            raise e
+    
+
 
 #Write a code to train model and check the accuracy.
 
